2fcda29a86739b0d50e4266f1f4e81cfefbbe0db
amelia <guaremaamelia@gmail.com>
2023-06-21 19:04:38 -0300

    modificaciones en navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,10 +3,17 @@ import React from 'react';
 const NavBar = () => {
     return(
 
-        <nav className="navbar bg-dark text-light mb-5">
-            <div className="container-fluid">
-                <h3 className="mx-auto">Pronóstico Climático</h3>
+        <nav className="navbar bg-black text-light mb-5">
+            <div className="container-fluid" >
+                <h3 className="">
+                  <img src="./Logo.png" alt="aqui hay un logo" width={140} />
+                    Pronóstico Climático
+                </h3>
+                <h1 className="">Home</h1>
+                <h1 className=''>Formulario</h1>
+                <h1 className=''>Sobre Nosotros</h1>
             </div>
+
         </nav>
 
     );
