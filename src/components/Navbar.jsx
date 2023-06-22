19ed673781d02bbcbd22bf7bb07476902b8f78e8
amelia <guaremaamelia@gmail.com>
2023-06-22 18:08:20 -0300

    cambios en la navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,6 +6,7 @@ const NavBar = () => {
         <nav className="navbar bg-dark text-light mb-5">
             <div className="container-fluid">
                 <h3 className="mx-auto">Pronóstico Climático</h3>
+                <h1>hola mundo</h1>
             </div>
         </nav>
 
