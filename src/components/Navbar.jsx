cb93ac0501021202883ad319027400355381d565
amelia <guaremaamelia@gmail.com>
2023-06-21 22:28:11 -0300

    modoficaciones

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,27 @@
 import React from 'react';
 
 const NavBar = () => {
-    return(
-
-        <nav className="navbar bg-black text-light mb-5">
-            <div className="container-fluid" >
-                <h3 className="">
-                  <img src="./Logo.png" alt="aqui hay un logo" width={140} />
-                    Pronóstico Climático
-                </h3>
-                <h1 className="">Home</h1>
-                <h1 className=''>Formulario</h1>
-                <h1 className=''>Sobre Nosotros</h1>
-            </div>
-
-        </nav>
-
-    );
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-black text-light mb-5">
+      <div className="container-fluid">
+        <h3 className="navbar-brand">
+          <img src="./Logo.png" alt="aqui hay un logo" width={140} />
+          Pronóstico Climático
+        </h3>
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <a className="nav-link" href="#">Home</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">Formulario</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">Sobre Nosotros</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 }
 
 export default NavBar;
