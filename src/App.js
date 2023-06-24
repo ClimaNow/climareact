5112c75568ed073cf5364c9991af4c8d033a4690
amelia <guaremaamelia@gmail.com>
2023-06-24 00:31:19 -0300

    Cambios en el Footer

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import Home from './components/Home';
 import LandingPage from './components/LandingPage';
 import WeatherPanel from './components/WeatherPanel';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           <Route exact path="/about" element={<h2>Sobre Nosotros</h2>} /> {/* Agrega la ruta para "About Us" */}
           <Route exact path="/formulario" element={<h2>Formulario de Contacto</h2>} /> {/* Agrega la ruta para "Contact Form" */}
           <Route exact path="/weather" element={<WeatherPanel />} />
+          <Route exact path='/Footer' element= {<Footer /> } />
         </Routes>
       </div>
     </BrowserRouter>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,41 @@
 import React from 'react';
 
 
-const Footer = ( ) => {
-return (
-    <footer>
-        <p> Pie de pagina</p>
-    </footer>
-);
+
+const Footer = () => {
+  return (
+    
+    <div id="container3"> 
+
+      <footer classpie="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+        <div classpie="col-md-4 d-flex align-items-center">
+            <span classpie="mb-3 mb-md-0 text-body-secondary">
+                <h4 id='titulo-footer'>Clima-Now</h4>
+                <h4 id='derechos'>Derechos Reservados. Copyright © 2023</h4>
+            </span>
+        </div>
+        
+        <div className='col-sm-2 col-lg-4 col-xs-12 footer-text-contac text-center' align='text-center'>
+            <div id="list-footer">
+                <span className='font-footer'>  
+                <h6>Contacto</h6>
+                </span>
+                <h6>Dirección: CABA-Argentina</h6>
+                <h6>Clima-Now@gmail.com</h6>    
+            </div>
+        </div>
+        <div id='container2'>
+         <ul classpie="nav col-md-4 list-unstyled d-flex">
+            <button> <a href="https://twitter.com/" > <img src="./img/Twitter.jpg" alt="instagram" width='50px' height='50px'/> </a></button>
+            <button>  <a href="https://www.instagram.com/">  <img src="./img/Instagram (2).jpg" alt="instagram" width='50px' height='50px'/> </a></button>
+            <button> <a href="https://www.facebook.com/"> <img src="./img/Facebook.jpg" alt="instagram" width='50px' height='50px'/> </a></button>
+ 
+         </ul>
+        </div>
+
+     </footer>
+    </div>
+  )
 }
 
-export default Footer;
+export default Footer
