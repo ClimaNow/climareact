17d0be0bfbc289f7361bbe9ac5e4b60f3d8afec9
scarllet_Ohara1968 <134315443+ClimaNow@users.noreply.github.com>
2023-06-26 05:21:02 -0300

    estilos

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,68 +1,45 @@
 import React from 'react';
-import Spinner from './Spinner';
-import './Card.css';
+import Navbar from './Navbar'; 
 
-const Card = ({ loadingData, showData, weather, forecast }) => {
-  if (loadingData) {
-    return <Spinner />;
-  }
-
-  if (!showData || !weather || !weather.location) {
-    return <h2 className="text-light">Buscador</h2>;
-  }
-
-  const today = new Date().toLocaleDateString('es-AR');
-
-  const renderForecastItem = (item) => {
-    const forecastDate = new Date(item.date).toLocaleString('es-AR', { hour: '2-digit', minute: '2-digit' });
-
-    return (
-      <div className="col" key={item.date}>
-        <p>{forecastDate}</p>
-        <p className="description">
-          <img src={item.day.condition.icon} alt="icon" />
-          {item.day.condition.text}
-        </p>
-        <p className="temp">{item.day.maxtemp_c.toFixed(1)}ºC</p>
-      </div>
-    );
-  };
-
-  const locationName = weather.location ? weather.location.name : '';
+const About = () => {
+  const personas = [
+    {
+      nombre: 'Amelia Guarema',
+      imagen: './Assets/amelia.jpg',
+      descripcion: 'Programadora en desarrollo.',
+    },
+    {
+      nombre: 'Scarlet Lugo',
+      imagen: './Assets/scarlet.jpg',
+      descripcion: 'Programadora en Desarrollo',
+    },
+    {
+      nombre: 'Yuliana Camacho',
+      imagen: './Assets/yuliana.jpg',
+      descripcion: 'Programadora en desarrollo',
+    },
+  ];
 
   return (
-    <div className="mt-5 d-flex justify-content-center align-items-center" style={{ marginTop: '100px' }}>
+    <div>
+      <Navbar />
       <div className="container">
-        <div className="card mb-3 mx-auto bg-dark text-light custom-card" style={{ height: '500px', width: '300px' }}>
-          <div className="row g-0 align-items-center">
-            <div className="col-md-4">
-              <h3 className="card-title">{locationName}</h3>
-              <p className="card-date">{today}</p>
-              <h1 className="card-temp">{weather.current.temp_c.toFixed(1)}ºC</h1>
-              <p className="card-desc">
-                <img src={weather.current.condition.icon} alt="icon" />
-                {weather.current.condition.text}
-              </p>
-              <img src={weather.current.condition.icon} className="img-fluid rounded-start" alt="..." />
-            </div>
-            <div className="col-md-8">
-              <div className="card-body text-start mt-2"style={{ marginTop: '100px' }}>
-                <h5 className="card-text">Temperatura máxima: {weather.forecast.forecastday[0].day.maxtemp_c.toFixed(1)}ºC</h5>
-                <h5 className="card-text">Temperatura mínima: {weather.forecast.forecastday[0].day.mintemp_c.toFixed(1)}ºC</h5>
-                <h5 className="card-text">Sensación térmica: {weather.current.feelslike_c.toFixed(1)}ºC</h5>
-                <h5 className="card-text">Humedad: {weather.current.humidity}%</h5>
-                <h5 className="card-text">Velocidad del viento: {weather.current.wind_kph} km/h</h5>
-              </div>
-              <hr />
-              <div className="row mt-4">
-                {forecast.forecastday.slice(0, 3).map((item) => renderForecastItem(item))}
+        <h1 className="text-primary">Acerca de Nosotros</h1>
+
+        <div className="row" style={{ display: 'grid', gridTemplateColumns: 'repeat(auto-fit, minmax(200px, 1fr))', gridGap: '1rem' }}>
+          {personas.map((persona, index) => (
+            <div className="card" key={index}>
+              <img src={persona.imagen} className="card-img-top" alt={persona.nombre} />
+              <div className="card-body">
+                <h5 className="card-title">{persona.nombre}</h5>
+                <p className="card-text" style={{ marginTop: '4rem' }}>{persona.descripcion}</p>
               </div>
             </div>
-          </div>
+          ))}
         </div>
       </div>
     </div>
   );
-}
+};
 
-export default Card;
+export default About;
